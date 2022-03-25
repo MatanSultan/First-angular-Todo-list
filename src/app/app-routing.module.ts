a6f902e041cdc036faa8342ad00846fd009d46b7
Matan sultan <88273616+MatanSultan@users.noreply.github.com>
2022-03-25 09:15:21 +0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+
+import { TodoComponent } from './todo/todo.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+{path: '', component: TodoComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
